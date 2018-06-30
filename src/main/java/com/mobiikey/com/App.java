1ec295c31014d38ed8040e54e865f7b667005495
mahesh <mahesh@gmail.com>
2018-07-01 03:09:30 +0530

    added src and pom

---
+++ src/main/java/com/mobiikey/com/App.java
@@ -0,0 +1,13 @@
+package com.mobiikey.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
